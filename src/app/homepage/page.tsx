11215c95587da89a86dcf21cd75bf7d4b8ca23eb
willystout <123514204+willystout@users.noreply.github.com>
2024-11-17 21:44:39 -0800

    fix on issue 19

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-import Head from 'next/head'
 import SearchForm from '@/components/SearchForm'
 
 export default function HomePage() {
@@ -8,15 +7,8 @@ export default function HomePage() {
     }
 
     return (
-        <>
-            <Head>
-                <title>GEO AI</title>
-                <meta name="description" content="GEO AI GIS APPLICATION" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-            </Head>
-            <main>
-                <SearchForm onSearch={handleSearch} placeholder="Please enter your query..." />
-            </main>
-        </>
+        <main>
+            <SearchForm onSearch={handleSearch} placeholder="Please enter your query..." />
+        </main>
     )
 }
