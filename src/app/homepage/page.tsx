cdd8855dd3ff11117b4702189f644b8c94add768
William Stout <wcstout@cadejo.cs.usfca.edu>
2024-12-04 00:24:32 -0800

    fixed server side rendering

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -1,36 +1,15 @@
-'use client';
-
-import Header from '@/components/Header'
-import { useEffect } from 'react';
-import SearchForm from '@/components/SearchForm'
+import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import fetchQueries from '@/supabase/supabaseClient';
+import SearchWrapper from '@/components/SearchWrapper';
 
 export default function HomePage() {
-    // useEffect(() => {
-    //     const logQueries = async () => {
-    //         const { countries, error } = await fetchQueries();
-    //         if (error) {
-    //             console.error('Error fetching queries:', error);
-    //         } else {
-    //             console.log('Fetched queries:', countries);
-    //         }
-    //     };
-
-    //     logQueries();
-    // }, []);
-
-    const handleSearch = (query: string) => {
-        console.log("User searched for:", query)
-    }
-
     return (
         <>
             <Header />
             <div className="w-full min-h-screen">
-                <SearchForm onSearch={handleSearch} placeholder="Please enter your query..." />
-                <Footer /> { }
+                <SearchWrapper />
+                <Footer />
             </div>
         </>
-    )
+    );
 }
